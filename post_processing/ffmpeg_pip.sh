bae817a1f6d094b35f573d7950922d1ebe85642f
Michelle Rossouw <michelleros128@gmail.com>
2024-04-02 00:33:33 +1100

    Added transparency to top right PIP

--- post_processing/ffmpeg_pip.sh
+++ post_processing/ffmpeg_pip.sh
@@ -4,11 +4,13 @@
     echo "Usage: RGB THERMAL FLIGHT OUTPUT"
     exit 1
 }
-	       
+
 
 RGB="$1"
 THERMAL="$2" # top left PIP
 FLIGHT="$3" # top right PIP
 OUTPUT="$4"
 
-ffmpeg -i $RGB -i $THERMAL -i $FLIGHT -filter_complex 'overlay=0:0,overlay=main_w-overlay_w:0' $OUTPUT
+ffmpeg -i $RGB -i $THERMAL -i $FLIGHT -filter_complex "[2:v]format=argb,geq=r='r(X,Y)':a='0.8*alpha(X,Y)'[al];
+[0:v][1:v]overlay=0:0[o1];
+[o1][al]overlay=main_w-overlay_w:0" $OUTPUT
