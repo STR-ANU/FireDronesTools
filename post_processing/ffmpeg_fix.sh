034a9ae90d7714dad4e1ad3e4214642c84038c1f
Michelle Rossouw <michelleros128@gmail.com>
2024-04-08 16:11:31 +1000

    Fixed ffmpeg_fix.sh to actually do the conversion.

--- post_processing/ffmpeg_fix.sh
+++ post_processing/ffmpeg_fix.sh
@@ -11,11 +11,9 @@ if [ "$#" -lt 2 ] ; then
     exit 1
 elif [ ! -z "$3" ] && [ "$3" = "m" ] ; then
     echo "Doing fix for mts recordings (just codec copy)."
-    echo "1 $1 2 $2"
-    # ffmpeg -i "$1" -c:v copy "$2"
+    ffmpeg -i "$1" -c:v copy "$2"
 else
     echo "Doing fix for playing on Chrome & Firefox (faststart and yuv420p)."
-    echo "1 $1 2 $2"
-    # ffmpeg -i "$1" -movflags faststart -pix_fmt yuv420p "$2"
+    ffmpeg -i "$1" -movflags faststart -pix_fmt yuv420p "$2"
 fi
 
